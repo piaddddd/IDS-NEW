f75a52eeefae17f34ac37c7398ee392c1705f3e4
piaddddd <150234712+piaddddd@users.noreply.github.com>
2026-05-20 10:04:48 +0800

    Update conn.php

--- conn.php
+++ conn.php
@@ -1,13 +1,11 @@
 <?php
 $servername = "localhost";
-$username = "root";  // Change this to your database username
-$password = "";      // Change this to your database password
-$db = "app_db";  // Replace with your database name
+$username = "root";  
+$password = "";      
+$db = "app_db"; 
 
-// Create connection
 $conn = new mysqli($servername, $username, $password, $db);
 
-// Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
